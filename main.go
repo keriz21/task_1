adcbb6bbb12c8852e2fb578d22872084dc7effe5
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2026-01-25 21:44:57 +0700

    update port

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ http.HandleFunc("/categories/", func(w http.ResponseWriter, r *http.Request) {
 	}
 })
 
-err:=http.ListenAndServe(":8000",nil)
+err:=http.ListenAndServe(":8080",nil)
 
 if err != nil{
 	fmt.Print(("gagal running server"))
